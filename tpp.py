d637e8b012876e6fccd5ce3ed61bcd73356b62bd
kpantemarco-star <249970349+kpantemarco-star@users.noreply.github.com>
2026-09-23 12:51:08 -0400

    commit 1

--- tpp.py
+++ tpp.py
@@ -19,26 +19,41 @@ equipe=os.path.join(os.getcwd(),"equipages.txt")
 
 
 # exercice5
-nom_yole = input("entrer le nom de la yole ")
-while nom_yole != "" :
-    commune_yole = input("quelle est la commune de votre yole: ")
-    prenom_patron = input("quelle est le nom de votre patron: ")
-    nombre_equipage = input("quelle est le nombre de votre equipages: ")
+# nom_yole = input("entrer le nom de la yole ")
+# while nom_yole != "" :
+#     commune_yole = input("quelle est la commune de votre yole: ")
+#     prenom_patron = input("quelle est le nom de votre patron: ")
+#     nombre_equipage = input("quelle est le nombre de votre equipages: ")
 
-    with open(equipe,"a") as f:
-        f.writelines(nom_yole + ";" + commune_yole + ";" + prenom_patron + ";" + nombre_equipage + ";")
+#     with open(equipe,"a") as f:
+#         f.writelines(nom_yole + ";" + commune_yole + ";" + prenom_patron + ";" + nombre_equipage + ";")
 
-    nom_yole = input("mettez un nom pour continuer laisser vide pour terminer ")
+#     nom_yole = input("mettez un nom pour continuer laisser vide pour terminer ")
 
-with open(equipe, "r") as f:
-    contenue = f.read()
+# with open(equipe, "r") as f:
+#     contenue = f.read()
+# print(contenue)
+
+
+# with open(equipe, "r") as f:
+#     n = 0 
+#     lignes = f.readline()
+#     while lignes != "":
+#         n+=1
+#         lignes = f.readline()
+# print(n)
+
+
+# exercice6
+communiquer = os.path.join(os.getcwd(), "communique.txt")
+with open(communiquer, "r") as c:
+    contenue = c.read()
+# print(repr(contenue))
 print(contenue)
-
-
-with open(equipe, "r") as f:
-    n = 0 
-    lignes = f.readline()
-    while lignes != "":
-        n+=1
-        lignes = f.readline()
-print(n)
+nom_yole = input("quelle est le nom de votre yole: ")
+commune_yole = input("quelle est la commune de votre yole: ")
+prenom_patron = input("quelle est le nom de votre patron: ")
+contenue = contenue.replace("la yole gagnante", nom_yole)
+contenue = contenue.replace("Le patron", prenom_patron)
+contenue = contenue.replace("la commune", commune_yole)
+print(contenue)
